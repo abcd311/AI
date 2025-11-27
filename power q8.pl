76bee00434ba46f48d95a55bb8c048084af7017e
abcd311 <codeexplorer24@gmail.com>
2025-11-28 00:47:25 +0530

    Update power q8.pl

--- power q8.pl
+++ power q8.pl
@@ -5,3 +5,15 @@ power(Num, Pow, Ans):-
     P1 is Pow-1,
     power(Num, Pl, Ans1),
     Ans is Num*Ans1.
+
+
+    code:
+    % Base case
+power(_, 0, 1).
+
+% Recursive case
+power(Num, Pow, Ans) :-
+    Pow > 0,
+    P1 is Pow - 1,
+    power(Num, P1, Ans1),
+    Ans is Num * Ans1.
